99c91b5ade655b7a2f71232d3a4cfafb6d1b967b
pangjiping.pjp <pangjiping.pjp@alibaba-inc.com>
2022-07-11 14:26:11 +0800

    add client-go usage example

---
+++ client_go/client.go
@@ -0,0 +1,189 @@
+package client_go
+
+import (
+	"context"
+	"flag"
+	v1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes/scheme"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/homedir"
+	"path/filepath"
+)
+
+// getK8sClientByKubeConfig 从本地kubeconfig文件中初始化k8sClient [grpc]
+// kubeconfig地址: ~/.kube/config
+func getK8sClientByKubeConfig() (*kubernetes.Clientset, error) {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+	k8sClient, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	return k8sClient, nil
+}
+
+// getRestClient 从本地kubeconfig文件初始化restfulClient [http]
+// kubeconfig地址: ~/.kube/config
+func getRestClient() (*rest.RESTClient, error) {
+	var kubeconfig *string
+	if home := homedir.HomeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+	} else {
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	}
+	flag.Parse()
+
+	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	if err != nil {
+		return nil, err
+	}
+
+	config.APIPath = "api"
+	config.GroupVersion = &v1.SchemeGroupVersion
+	config.NegotiatedSerializer = scheme.Codecs
+
+	restClient, err := rest.RESTClientFor(config)
+	if err != nil {
+		return nil, err
+	}
+	return restClient, nil
+}
+
+// getNodeFromApiServer 从apiserver获取node信息
+// 使用grpc，获取node列表
+// 对于大集群，如果不对etcd做优化的话，绝对不应该访问etcd，前提是不要求强一致性
+func getNodesFromApiServer() ([]v1.Node, error) {
+	k8sClient, err := getK8sClientByKubeConfig()
+	if err != nil {
+		return nil, err
+	}
+	nodeList, err := k8sClient.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{
+		LabelSelector:        "",             // 节点labelSelector,用"key=val"或者"key!=val"和","拼接成的字符串
+		FieldSelector:        "",             // 和label同理
+		ResourceVersion:      "0",            // 0 表示从apiserver获取最新缓存，不会打到etcd
+		ResourceVersionMatch: "NotOlderThan", // Match和resourceVersion的搭配见 https://kubernetes.io/zh-cn/docs/reference/using-api/api-concepts/
+		//TimeoutSeconds:       nil,            // 访问超时时间
+		//Limit:                1,              // 获取全量nodes没啥用,在获取nodeSize时有大用
+	})
+	if err != nil {
+		return nil, err
+	}
+	return nodeList.Items, nil
+}
+
+// getNodesWithPagination 从apiserver全量list node的一种优化
+// 具体使用见 describeNodesFromApiServer
+func getNodesWithPagination(nextToken string, maxResult int64) (*v1.NodeList, error) {
+	k8sClient, err := getK8sClientByKubeConfig()
+	if err != nil {
+		return nil, err
+	}
+	nodeList, err := k8sClient.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{
+		Continue:             nextToken,
+		Limit:                maxResult,
+		ResourceVersion:      "0",
+		ResourceVersionMatch: "NotOlderThan",
+	})
+	if err != nil {
+		return nil, err
+	}
+	return nodeList, nil
+}
+
+// describeNodesFromApiServer 从apiserver全量list node
+// 针对大集群的一种优化手段
+func describeNodesFromApiServer() ([]v1.Node, error) {
+	var nextToken = ""
+	var step int64 = 100
+	var totalCount int64
+	nodeList := make([]v1.Node, 0)
+
+	for {
+		nodes, err := getNodesWithPagination(nextToken, step)
+		if err != nil {
+			return nil, err
+		}
+		nodeList = append(nodeList, nodes.Items...)
+		totalCount += int64(len(nodes.Items))
+		if nodes.Continue == "" {
+			break
+		}
+		nextToken = nodes.Continue
+	}
+	return nodeList, nil
+}
+
+// getNodeSizeFromApiServer 只获取nodeSize信息，不需要元数据
+// 使用limit和remainingCount做优化，返回最少的数据
+func getNodeSizeFromApiServer() (int64, error) {
+	k8sClient, err := getK8sClientByKubeConfig()
+	if err != nil {
+		return 0, err
+	}
+	nodeList, err := k8sClient.CoreV1().Nodes().List(context.Background(), metav1.ListOptions{
+		Limit:                1,
+		ResourceVersion:      "0",
+		ResourceVersionMatch: "NotOlderThan",
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	size := *nodeList.RemainingItemCount + int64(len(nodeList.Items))
+	// 注意nodeList.Size()不是node的size, 而是返回的nodeList数据的大小, 多少字节
+	return size, nil
+}
+
+// getPodsFromApiServer 获取pods列表
+// 有很多可选择的优化项, 某个namespace、某个节点、某些标签
+// pod不像node, 尽量不要全量list
+func getPodsFromApiServer() ([]v1.Pod, error) {
+	k8sClient, err := getK8sClientByKubeConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	// 对于labelSelector和fieldSelector如果不知道可以用kubectl看一下具体的字段含义
+	podList, err := k8sClient.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{
+		LabelSelector:        "",
+		FieldSelector:        "spec.nodeName=nodeName", // 这个list某个节点上的
+		ResourceVersion:      "0",
+		ResourceVersionMatch: "NotOlderThan",
+	})
+	if err != nil {
+		return nil, err
+	}
+	return podList.Items, nil
+}
+
+// getPodSizeFromApiServer 获取podSize
+// 使用limit和remainingCount优化
+func getPodSizeFromApiServer() (int64, error) {
+	k8sClient, err := getK8sClientByKubeConfig()
+	if err != nil {
+		return 0, err
+	}
+
+	podList, err := k8sClient.CoreV1().Pods("").List(context.Background(), metav1.ListOptions{
+		ResourceVersion:      "0",
+		ResourceVersionMatch: "NotOlderThan",
+		Limit:                1,
+	})
+	if err != nil {
+		return 0, err
+	}
+	return *podList.RemainingItemCount + int64(len(podList.Items)), nil
+}
---
+++ client_go/client_test.go
@@ -0,0 +1,87 @@
+package client_go
+
+import (
+	"github.com/docker/machine/Godeps/_workspace/src/github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func Test_InitClientGo(t *testing.T) {
+	testcases := []map[string]interface{}{
+		{
+			"description":  "grpc client test",
+			"type":         "grpc",
+			"wantErr":      false,
+			"clientNotNil": true,
+		},
+		{
+			"description":  "rest client test",
+			"type":         "restful",
+			"wantErr":      false,
+			"clientNotNil": true,
+		},
+	}
+
+	var client interface{}
+	var err error
+
+	for _, tc := range testcases {
+		t.Log(tc["description"])
+		if tc["type"].(string) == "grpc" {
+			client, err = getK8sClientByKubeConfig()
+		} else {
+			client, err = getRestClient()
+		}
+		assert.Equal(t, tc["wantErr"].(bool), err != nil)
+		assert.Equal(t, tc["clientNotNil"].(bool), client != nil)
+	}
+}
+
+func Test_AllOpteration(t *testing.T) {
+
+	// init grpc client
+	t.Log("test for init grpc client ...")
+	k8sClient, err := getK8sClientByKubeConfig()
+	assert.Nil(t, err)
+	assert.NotNil(t, k8sClient)
+
+	// init restful client
+	t.Log("test for init restful client ...")
+	restClient, err := getRestClient()
+	assert.Nil(t, err)
+	assert.NotNil(t, restClient)
+
+	// get nodes
+	t.Log("test for get nodes from apiserver ...")
+	nodesOne, err := getNodesFromApiServer()
+	assert.Nil(t, err)
+	assert.NotNil(t, nodesOne)
+	assert.Equal(t, true, len(nodesOne) >= 1)
+
+	// get nodes
+	t.Log("test for get nodes from apiserver with optimizing...")
+	nodesTwo, err := describeNodesFromApiServer()
+	assert.Nil(t, err)
+	assert.NotNil(t, nodesTwo)
+	assert.Equal(t, true, len(nodesTwo) >= 1)
+
+	// get nodeSize
+	t.Log("test for get nodeSize from apiserver ...")
+	nodeSize, err := getNodeSizeFromApiServer()
+	assert.Nil(t, err)
+	assert.NotNil(t, nodeSize)
+	assert.Equal(t, true, nodeSize >= 1)
+
+	// get pods
+	t.Log("test for get pods from apiserver ...")
+	pods, err := getPodsFromApiServer()
+	assert.Nil(t, err)
+	assert.NotNil(t, pods)
+	assert.Equal(t, true, len(pods) >= 1)
+
+	// get podSize
+	t.Log("test for get podSize from apiserver ...")
+	podSize, err := getPodSizeFromApiServer()
+	assert.Nil(t, err)
+	assert.NotNil(t, podSize)
+	assert.Equal(t, true, podSize)
+}
